b39123dc7f6dd131f1cc4502c01f31c5867c67d5
nvhhung <hungnvh2701@gmail.com>
2022-08-16 15:40:40 +0700

    add login

---
+++ src/main/java/hcmut/cse/travelsocialnetwork/command/CommandLogin.java
@@ -0,0 +1,5 @@
+package hcmut.cse.travelsocialnetwork.command;/**
+ * @author : hung.nguyen23
+ * @since : 8/16/22 Tuesday
+ **/public class CommandLogin {
+}
---
+++ src/main/java/hcmut/cse/travelsocialnetwork/model/LoginToken.java
@@ -0,0 +1,5 @@
+package hcmut.cse.travelsocialnetwork.model;/**
+ * @author : hung.nguyen23
+ * @since : 8/16/22 Tuesday
+ **/public class LoginToken {
+}
