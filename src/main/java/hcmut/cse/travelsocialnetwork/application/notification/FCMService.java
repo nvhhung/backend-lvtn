3243d09a13c9e9a69278e02eaea8c21104567150
nvhhung <hungnvh2701@gmail.com>
2022-11-21 22:51:24 +0700

    add

--- src/main/java/hcmut/cse/travelsocialnetwork/application/notification/FCMService.java
+++ src/main/java/hcmut/cse/travelsocialnetwork/application/notification/FCMService.java
@@ -1,8 +1,8 @@
 package hcmut.cse.travelsocialnetwork.application.notification;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
 import hcmut.cse.travelsocialnetwork.application.comment.CommentApplication;
-import hcmut.cse.travelsocialnetwork.model.PushNotificationRequest;
-import hcmut.cse.travelsocialnetwork.utils.JSONUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -21,5 +21,12 @@ public class FCMService {
 //        var response = send
 //    }
 //
-//    private String sendAndGetResponse(Message message)
+    private String sendAndGetResponse(Message message) {
+        try{
+            return FirebaseMessaging.getInstance().sendAsync(message).get();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return "";
+        }
+    }
 }
