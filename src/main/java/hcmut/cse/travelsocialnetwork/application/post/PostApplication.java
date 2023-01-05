62477241f4a4171f0526d6980728e6b2894db6d6
nvhhung <hungnvh2701@gmail.com>
2023-01-05 21:29:52 +0700

    optimize import

--- src/main/java/hcmut/cse/travelsocialnetwork/application/post/PostApplication.java
+++ src/main/java/hcmut/cse/travelsocialnetwork/application/post/PostApplication.java
@@ -24,7 +24,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
